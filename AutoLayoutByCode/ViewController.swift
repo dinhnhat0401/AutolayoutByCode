c1fe9f30381abba5685d5a0cec1fb9afb4a0d5f5
dinhnhat0401 <nhatphutho@gmail.com>
2019-04-10 23:33:10 +0900

    wip

--- AutoLayoutByCode/ViewController.swift
+++ AutoLayoutByCode/ViewController.swift
@@ -18,34 +18,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+        self.view.addSubview(containerView)
 
         containerView.backgroundColor = .blue
         containerView.addSubview(scrollView)
         
-        for i in 0...2 {
-            let tmpView = UIView()
-            contentViews.append(tmpView)
-            configContentView(for: contentViews[i])
-            
-//            NSLayoutConstraint.constraints(withVisualFormat: "|imageView|", options: [], metrics: nil, views: views)
+//        for i in 0...2 {
+//            let tmpView = UIView()
+//            containerView.addSubview(tmpView)
+//            contentViews.append(tmpView)
+//            configContentView(for: contentViews[i])
+//        }
+        
+        let views = ["containerView": containerView,
+                     "scrollView": scrollView
+//                     "contentView1": contentViews[0],
+//                     "contentView2": contentViews[1],
+//                     "contentView3": contentViews[2]
+        ]
+        for (_, view) in views {
+            view.translatesAutoresizingMaskIntoConstraints = false
         }
+        
+        NSLayoutConstraint.constraints(withVisualFormat: "H:|-0@750-[containerView]-0@750-|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.constraints(withVisualFormat: "V:|-0@750-[containerView]-0@750-|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.constraints(withVisualFormat: "|[scrollView]|", options: [.alignAllLeading, .alignAllTrailing, .alignAllTop, .alignAllBottom, .alignAllCenterX, .alignAllCenterY], metrics:nil, views: views)
+//        NSLayoutConstraint.constraints(withVisualFormat: "|[contentView1][contentView2][contentView3]|", options: [], metrics: nil, views: views)
     }
     
     private func configContentView(for view: UIView) {
         let imageView = UIImageView(image: UIImage(named: "harry-dinh"))
+        imageView.contentMode = .scaleAspectFit
         let description = UILabel()
         
         view.addSubview(imageView)
         view.addSubview(description)
         
-        let views: [String: UIView] = ["view": view, "imageView": imageView, "description": description]
+        let views = ["view": view, "imageView": imageView, "description": description]
 
-        for _ in views {
+        for (_, view) in views {
             view.translatesAutoresizingMaskIntoConstraints = false
         }
         
-        NSLayoutConstraint.constraints(withVisualFormat: "|imageView|", options: [], metrics: nil, views: views)
-        NSLayoutConstraint.constraints(withVisualFormat: "V:|imageView", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.constraints(withVisualFormat: "|[imageView]|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView]", options: [], metrics: nil, views: views)
         NSLayoutConstraint.constraints(withVisualFormat: "V:[description]|", options: [], metrics: nil, views: views)
         
     }
